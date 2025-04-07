f4d9a35262a74529a0ef950c98c37a8d2e41a832
jtrab-123 <j.trabert123@gmail.com>
2025-04-07 14:18:41 -0700

    Create dog_grooming_boarding.js

---
+++ js/dog_grooming_boarding.js
@@ -0,0 +1,14 @@
+const dogGroomingBoarding = [
+  {"Month": 'January', "Dogs Groomed": 18, "Dogs Boarded": 26},
+  {"Month": 'February', "Dogs Groomed": 44, "Dogs Boarded": 10},
+  {"Month": 'March', "Dogs Groomed": 10, "Dogs Boarded": 23},
+  {"Month": 'April', "Dogs Groomed": 27, "Dogs Boarded": 7},
+  {"Month": 'May', "Dogs Groomed": 40, "Dogs Boarded": 13},
+  {"Month": 'June', "Dogs Groomed": 26, "Dogs Boarded": 11},
+  {"Month": 'July', "Dogs Groomed": 35, "Dogs Boarded": 16},
+  {"Month": 'August', "Dogs Groomed": 13, "Dogs Boarded": 5},
+  {"Month": 'September', "Dogs Groomed": 32, "Dogs Boarded": 7},
+  {"Month": 'October', "Dogs Groomed": 28, "Dogs Boarded": 30},
+  {"Month": 'November', "Dogs Groomed": 10, "Dogs Boarded": 30},
+  {"Month": 'December', "Dogs Groomed": 13, "Dogs Boarded": 12},
+];

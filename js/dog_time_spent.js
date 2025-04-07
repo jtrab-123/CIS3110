9fc701dd39fbd918014ce724da524cfa3b0b3ffd
jtrab-123 <j.trabert123@gmail.com>
2025-04-07 14:20:09 -0700

    Create dog_time_spent.js

---
+++ js/dog_time_spent.js
@@ -0,0 +1,13 @@
+  {"Month": 'January', "Avg. Grooming Time (hours)": 3.6207317609258487, "Avg. Boarding Time (hours)": 5.244061180429994},
+  {"Month": 'February', "Avg. Grooming Time (hours)": 1.5423492886826162, "Avg. Boarding Time (hours)": 19.68845202950581},
+  {"Month": 'March', "Avg. Grooming Time (hours)": 2.1272811819219983, "Avg. Boarding Time (hours)": 15.20048596171016},
+  {"Month": 'April', "Avg. Grooming Time (hours)": 3.298758246478962, "Avg. Boarding Time (hours)": 18.738266759694085},
+  {"Month": 'May', "Avg. Grooming Time (hours)": 3.747029663653608, "Avg. Boarding Time (hours)": 19.458230700434427},
+  {"Month": 'June', "Avg. Grooming Time (hours)": 2.0144350272602867, "Avg. Boarding Time (hours)": 12.17546343017732},
+  {"Month": 'July', "Avg. Grooming Time (hours)": 2.3961666275721942, "Avg. Boarding Time (hours)": 17.40926847864253},
+  {"Month": 'August', "Avg. Grooming Time (hours)": 2.658292101826157, "Avg. Boarding Time (hours)": 7.82183840704559},
+  {"Month": 'September', "Avg. Grooming Time (hours)": 2.878782364229061, "Avg. Boarding Time (hours)": 16.791086683432205},
+  {"Month": 'October', "Avg. Grooming Time (hours)": 3.8425157117251088, "Avg. Boarding Time (hours)": 12.104198886829009},
+  {"Month": 'November', "Avg. Grooming Time (hours)": 1.6618086839049404, "Avg. Boarding Time (hours)": 13.68005794705884},
+  {"Month": 'December', "Avg. Grooming Time (hours)": 3.0578353603244754, "Avg. Boarding Time (hours)": 18.578738029440693},
+];

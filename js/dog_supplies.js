5a49b0531cb36888b3c48a2b2ee278b7694db39e
jtrab-123 <j.trabert123@gmail.com>
2025-04-07 14:19:43 -0700

    Create dog_supplies.js

---
+++ js/dog_supplies.js
@@ -0,0 +1,7 @@
+const dogSupplies = [
+  {"Month": 'January', "Grooming Supplies": 50, "Boarding Supplies": 100},
+  {"Month": 'February', "Grooming Supplies": 60, "Boarding Supplies": 120},
+  {"Month": 'March', "Grooming Supplies": 45, "Boarding Supplies": 90},
+  {"Month": 'April', "Grooming Supplies": 70, "Boarding Supplies": 110},
+  {"Month": 'May', "Grooming Supplies": 55, "Boarding Supplies": 130},
+];
